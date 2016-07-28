db7d4da577f85d2ffc356f6d10bd6a2d25ab8ba5
LycheeLisa <lisajia.chen@mail.utoronto.ca>
2016-07-28 14:01:12 -0400

    Changed jumbotron
    Added a jumbotron

--- js/customjs.js
+++ js/customjs.js
@@ -1,3 +1,13 @@
 /**
  * Created by chenji13 on 7/26/2016.
  */
+$(document).ready(function() {
+    var div = $("#About");
+    var pos = div.position();
+    $(window).scroll(function () {
+        var windowpos = $(window).scrollTop();
+        if (windowpos >= (pos.top - 100)) {
+            $(div).addClass("afterscroll");
+        }
+    });
+});
