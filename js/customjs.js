caea2ba6723bc1d9a753f09d3deefdcacd388b31
LycheeLisa <lisajia.chen@mail.utoronto.ca>
2016-07-28 15:03:24 -0400

    Changed loading time on box and removed loader
    Loader didn't work </3

--- js/customjs.js
+++ js/customjs.js
@@ -1,16 +1,12 @@
 /**
  * Created by chenji13 on 7/26/2016.
  */
-$(window).load(function(){
-    $("#loader").fadeOut("fast")
-});
 $(document).ready(function() {
     var div = $("#About");
     var pos = div.position();
     $(window).scroll(function () {
-        var windowpos = $(window).scrollTop();
-        if (windowpos >= (pos.top - 100)) {
-            $(div).addClass("afterscroll");
-        }
+        /**var windowpos = $(window).scrollTop();
+        if (windowpos >= (pos.top + 10)) {*/
+        $(div).addClass("afterscroll");
     });
 });
